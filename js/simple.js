086daff5c2692bf195c7422e546e1744b0fcfdd7
Sahiba Kumari <56750857+sahiba0915@users.noreply.github.com>
2020-06-27 20:11:48 +0530

    Initial commit

---
+++ js/simple.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "#6633FF", "rgb(204, 0, 102)", "yellow" ];
+const btn =document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+
+const randomNumber = getRandomNumber();
+console.log(randomNumber);
+document.body.style.backgroundColor = colors[randomNumber];
+color.textContent = colors[randomNumber];
+
+});
+
+function getRandomNumber() {
+    return Math.floor( Math.random()*colors.length);
+}
